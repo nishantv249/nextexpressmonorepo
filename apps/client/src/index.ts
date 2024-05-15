03a285ca9584fa366a28a34eb73bfe1f4e5f3cc1
nishant <nishantv249@gmail.com>
2024-05-15 14:56:09 +0530

    client

---
+++ apps/client/src/index.ts
@@ -0,0 +1,16 @@
+import next from 'next';
+import { resolve } from 'path';
+
+const dev = process.env.NODE_ENV !== 'production';
+let nextDir = __dirname;
+if (nextDir.indexOf('/dist') !== -1) {
+  nextDir = resolve(__dirname, '../client');
+}
+nextDir = resolve(__dirname, '../client');
+
+const nextapp = next({
+  dev,
+  dir: nextDir
+});
+
+export default nextapp;
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,7 +5,13 @@
 
 import express from 'express';
 import * as path from 'path';
+import next  from 'next';
+import nextapp from 'apps/client/src';
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const handle = nextapp.getRequestHandler();
+nextapp.prepare().then(() => {
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -14,8 +20,13 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to server!' });
 });
 
+app.get('*', (req, res) => {
+  handle(req, res);
+});
+
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
+})
